9f01ab1d9473a6af72df9c7ed89472add228b291
SVS250404 <shivanisatish2020@gmail.com>
2025-03-06 11:01:39 +0000

    my work

---
+++ shivani/test1.py
@@ -0,0 +1,10 @@
+def print_name_pattern(name):
+    for i in range(1, len(name) + 1):
+        print(name[:i])
+
+
+# Example usage
+name = "shivani"
+print_name_pattern(name)
+
+# You can replace "shivani" with any other name to test!
